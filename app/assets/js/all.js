5e00ae2432b90cb06f92a58936fa75cbcb52c64f
KAROL0820 <karolinee0820@gmail.com>
2022-09-07 14:34:44 +0800

    0907

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,41 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  $('.arrow').click(function (e) {
+    e.preventDefault();
+    $('.down').toggleClass('d-none');
+    $('.up').toggleClass('d-none');
+    
+  })
 });
+$(function() {
+  $('.arrow-2').click(function (e) {
+    e.preventDefault();
+    $('.down-2').toggleClass('d-none');
+    $('.up-2').toggleClass('d-none');
+    
+  })
+});
+$(function() {
+  $('.arrow-3').click(function (e) {
+    e.preventDefault();
+    $('.down-3').toggleClass('d-none');
+    $('.up-3').toggleClass('d-none');
+    
+  })
+});
+$(function() {
+  $('.arrow-4').click(function (e) {
+    e.preventDefault();
+    $('.down-4').toggleClass('d-none');
+    $('.up-4').toggleClass('d-none');
+    
+  })
+});
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
